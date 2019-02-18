563fc1214bdf2f01e7a107eb839106502ddca34c
Nfahndrich <02nicholas.fahndrich@gulllakecs.org>
2019-02-18 12:13:34 -0800

    Add Functions And Variables

--- Functions-git.py
+++ Functions-git.py
@@ -2,6 +2,7 @@
 # Nicholas Fahndrich
 # 2/5/19
 
+'''
 name = input("What is your name: ")
 
 def greeting():
@@ -9,3 +10,19 @@ def greeting():
     print("Nice to meet you")
 
 greeting()
+'''
+
+
+# 4.13.4: Functions And Variables
+# Nicholas Fahndrich
+# 2/18/19
+
+x = 6
+
+def print_something():
+    x=3
+    print (x)
+
+print_something()
+print (x)
+
